e11a05245f3e7dc424b08a1633f27138f1d4eb9e
polunwu <polunwu@gmail.com>
2021-05-30 22:14:22 +0800

    Storing user data in firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,33 @@ const config = {
   appId: '1:791343677060:web:f4641ba866df98cbd80cd1',
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return; // 沒有使用者資料
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`); // get documentRef
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    // database 尚未儲存該使用者資料
+    const { displayName, email } = userAuth;
+    const createdAt = new Date(); // 生成時間
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log('error creating user');
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
