6a50ed895408edf2a156900e50912913dd33aa1d
polunwu <polunwu@gmail.com>
2021-05-30 11:55:10 +0800

    Add ShopPage & CollectionPreview component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import { withRouter } from "react-router-dom";
+import React from 'react';
+import { withRouter, useRouteMatch } from 'react-router-dom';
 
-import './menu-item.styles.scss'
+import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
-  <div 
-    className={size ? `${size} menu-item` : 'menu-item'}
-    onClick={() => history.push(`${match.url}${linkUrl}`)}
-  >
-    <div 
-      className="background-image" 
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }} 
-    />
-    <div className='content'>
-      <h1 className='title'>{ title.toUpperCase() }</h1>
-      <span className='subtitle'>SHOP NOW</span>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
+  return (
+    <div
+      className={size ? `${size} menu-item` : 'menu-item'}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
+      <div
+        className='background-image'
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className='content'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+        <span className='subtitle'>SHOP NOW</span>
+      </div>
     </div>
-  </div>
-)
+  );
+};
 
-export default withRouter(MenuItem)
+export default withRouter(MenuItem);
