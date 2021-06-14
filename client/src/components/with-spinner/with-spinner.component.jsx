50dd4c66c3c5618e5b491a536648d5f4d9b2a45b
polunwu <polunwu@gmail.com>
2021-06-14 14:26:29 +0800

    Add route code splitting & Spinner component

--- client/src/components/with-spinner/with-spinner.component.jsx
+++ client/src/components/with-spinner/with-spinner.component.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 
-import { SpinnerOverlay, SpinnerContainer } from './with-spinner.styles';
+import Spinner from '../spinner/spinner.comonent';
 
 const WithSpinner = (WrappedComponent) => {
-  const Spinner = ({ isLoading, ...otherProps }) => {
-    return isLoading ? (
-      <SpinnerOverlay>
-        <SpinnerContainer />
-      </SpinnerOverlay>
-    ) : (
-      <WrappedComponent {...otherProps} />
-    );
+  const WithSpinnerComponent = ({ isLoading, ...otherProps }) => {
+    return isLoading ? <Spinner /> : <WrappedComponent {...otherProps} />;
   };
 
-  return Spinner;
+  return WithSpinnerComponent;
 };
 
 export default WithSpinner;
